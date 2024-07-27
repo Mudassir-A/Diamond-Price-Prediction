d6b4e04a6b14f2d5f819ea289b8ed349b9d36df2
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-07-27 15:11:21 +0530

    updated: requirements.txt setup.txt

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path: str) -> List[str]:
 
 
 setup(
-    name="Diamond Price Prediction",
+    name="DiamondPricePrediction",
     version="0.0.1",
     author="Mudassir Ansari",
     author_email="mudassirimranansari@gmail.com",
